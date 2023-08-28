17308df595e33a74fd80d8b3fa79bee657bb37f4
babucarr32 <baboucarry@gmai.com>
2023-08-28 11:27:34 +0000

    Defined the types of home route

--- dist/src/index.js
+++ dist/src/index.js
@@ -3,11 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-app.use((0, cookie_parser_1.default)());
-const port = process.env.PORT || 3000;
+// app.use(cookieParser());
+const port = process.env.PORT || 8080;
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+// import { ApolloServer } from "@apollo/server";
+// import { startStandaloneServer } from "@apollo/server/standalone";
 // import { typeDefs } from "./graphql/typedefs";
 // import { resolvers } from "./graphql/resolvers";
-import { connectDB } from "../utils/connectDB.js";
+// import { connectDB } from "../utils/connectDB.js";
 import cookieParser from "cookie-parser";
-import express from "express";
+import express, { Request, Response } from "express";
 import { data } from "../utils/db.js";
 
 const app = express();
-app.use(cookieParser());
+// app.use(cookieParser());
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
