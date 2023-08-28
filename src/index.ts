57b3eac33cfb9dac5f1fa27bb0ab64eecced4660
babucarr32 <baboucarry@gmai.com>
2023-08-28 11:29:24 +0000

    removed the comments in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,13 @@
-// import { ApolloServer } from "@apollo/server";
-// import { startStandaloneServer } from "@apollo/server/standalone";
-// import { typeDefs } from "./graphql/typedefs";
-// import { resolvers } from "./graphql/resolvers";
-// import { connectDB } from "../utils/connectDB.js";
-import cookieParser from "cookie-parser";
 import express, { Request, Response } from "express";
-import { data } from "../utils/db.js";
 
 const app = express();
-// app.use(cookieParser());
 
 const port = process.env.PORT || 8080;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.json("Hello World!");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-// async function connectToDatabase() {
-//   await connectDB();
-// }
-// // connectToDatabase();
-
-// const typeDefs = `#graphql
-// type User{
-//     username: String
-//     password: String
-// }
-
-// type Query{
-//     user: [User]
-// }
-// `;
-
-// const resolvers = {
-//   Query: {
-//     async user() {
-//       return data.users;
-//     },
-//   },
-// };
-
-// async function startApolloServer() {
-//   const server = new ApolloServer({
-//     typeDefs,
-//     resolvers,
-//   });
-//   const { url } = await startStandaloneServer(server, {
-//     context: async ({ req, res }) => ({ req, res }),
-//     listen: { port: 4000 },
-//   });
-//   console.log(`
-//     🚀  Server is running!
-//     📭  Query at ${url}
-//     `);
-// }
-// // startApolloServer();
