6a4c85564730333250ece4802dec54a538f36755
babucarr32 <baboucarry@gmai.com>
2023-08-28 12:23:46 +0000

    Brought back the apollo server

--- src/index.js
+++ src/index.js
@@ -3,58 +3,50 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 // import { typeDefs } from "./graphql/typedefs";
 // import { resolvers } from "./graphql/resolvers";
 import { connectDB } from "../utils/connectDB.js";
-// import cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser";
 import express from "express";
-// import { data } from "../utils/db.js";
+import { data } from "../utils/db.js";
 
 const app = express();
 
 const port = process.env.PORT || 8080;
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
 
 async function connectToDatabase() {
   await connectDB();
 }
 connectToDatabase();
 
-// const typeDefs = `#graphql
-// type User{
-//     username: String
-//     password: String
-// }
+const typeDefs = `#graphql
+type User{
+    username: String
+    password: String
+}
 
-// type Query{
-//     user: [User]
-// }
-// `;
+type Query{
+    user: [User]
+}
+`;
 
-// const resolvers = {
-//   Query: {
-//     async user() {
-//       return data.users;
-//     },
-//   },
-// };
+const resolvers = {
+  Query: {
+    async user() {
+      return data.users;
+    },
+  },
+};
 
-// async function startApolloServer() {
-//   const server = new ApolloServer({
-//     typeDefs,
-//     resolvers,
-//   });
-//   const { url } = await startStandaloneServer(server, {
-//     context: async ({ req, res }) => ({ req, res }),
-//     listen: { port: 4000 },
-//   });
-//   console.log(`
-//     🚀  Server is running!
-//     📭  Query at ${url}
-//     `);
-// }
-// // startApolloServer();
+async function startApolloServer() {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+  const { url } = await startStandaloneServer(server, {
+    context: async ({ req, res }) => ({ req, res }),
+    listen: { port: port },
+  });
+  console.log(`
+    🚀  Server is running!
+    📭  Query at ${url}
+    `);
+}
+startApolloServer();
