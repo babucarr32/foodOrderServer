c703e602aa8167891bd712f125fcecb2bc5315ab
babucarr32 <baboucarry@gmai.com>
2023-08-28 11:23:28 +0000

    updated index.ts again

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,16 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-// import { typeDefs } from "./graphql/typedefs";
-// import { resolvers } from "./graphql/resolvers";
-import { connectDB } from "../utils/connectDB.js";
-import cookieParser from "cookie-parser";
-import express from "express";
-import { data } from "../utils/db.js";
+import express, { Request, Response } from "express";
 
 const app = express();
-app.use(cookieParser());
+const port = process.env.PORT || 8080;
 
-const port = 3000;
+app.get("/", (_req: Request, res: Response) => {
+  return res.send("Express Typescript on Vercel");
+});
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/ping", (_req: Request, res: Response) => {
+  return res.send("pong 🏓");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  return console.log(`Server is listening on ${port}`);
 });
-
-// async function connectToDatabase() {
-//   await connectDB();
-// }
-// // connectToDatabase();
-
-// const typeDefs = `#graphql
-// type User{
-//     username: String
-//     password: String
-// }
-
-// type Query{
-//     user: [User]
-// }
-// `;
-
-// const resolvers = {
-//   Query: {
-//     async user() {
-//       return data.users;
-//     },
-//   },
-// };
-
-// async function startApolloServer() {
-//   const server = new ApolloServer({
-//     typeDefs,
-//     resolvers,
-//   });
-//   const { url } = await startStandaloneServer(server, {
-//     context: async ({ req, res }) => ({ req, res }),
-//     listen: { port: 4000 },
-//   });
-//   console.log(`
-//     🚀  Server is running!
-//     📭  Query at ${url}
-//     `);
-// }
-// // startApolloServer();
