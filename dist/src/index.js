2d2f344cfe62f47c64aa016732ef617110f97b2a
babucarr32 <baboucarry@gmai.com>
2023-08-28 11:25:01 +0000

    updated index.ts again

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,41 +1,32 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-// import { typeDefs } from "./graphql/typedefs";
-// import { resolvers } from "./graphql/resolvers";
-import { connectDB } from "../utils/connectDB.js";
-import cookieParser from "cookie-parser";
-import express from "express";
-import { data } from "../utils/db.js";
-
-const app = express();
-app.use(cookieParser());
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.use((0, cookie_parser_1.default)());
 const port = process.env.PORT || 3000;
-
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Hello World!");
 });
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
-
 // async function connectToDatabase() {
 //   await connectDB();
 // }
 // // connectToDatabase();
-
 // const typeDefs = `#graphql
 // type User{
 //     username: String
 //     password: String
 // }
-
 // type Query{
 //     user: [User]
 // }
 // `;
-
 // const resolvers = {
 //   Query: {
 //     async user() {
@@ -43,7 +34,6 @@ app.listen(port, () => {
 //     },
 //   },
 // };
-
 // async function startApolloServer() {
 //   const server = new ApolloServer({
 //     typeDefs,
@@ -59,3 +49,4 @@ app.listen(port, () => {
 //     `);
 // }
 // // startApolloServer();
+//# sourceMappingURL=index.js.map
