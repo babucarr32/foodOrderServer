4bce87ef3dbaf870e6121d9c137b9ca5c2fcebea
babucarr32 <baboucarry@gmai.com>
2023-08-28 11:19:58 +0000

    updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 // import { typeDefs } from "./graphql/typedefs";
 // import { resolvers } from "./graphql/resolvers";
-import { connectDB } from "../utils/connectDB";
+import { connectDB } from "../utils/connectDB.js";
 import cookieParser from "cookie-parser";
 import express from "express";
-import { data } from "../utils/db";
+import { data } from "../utils/db.js";
 
 const app = express();
 app.use(cookieParser());
@@ -20,42 +20,42 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-async function connectToDatabase() {
-  await connectDB();
-}
-// connectToDatabase();
+// async function connectToDatabase() {
+//   await connectDB();
+// }
+// // connectToDatabase();
 
-const typeDefs = `#graphql 
-type User{
-    username: String
-    password: String
-}
+// const typeDefs = `#graphql
+// type User{
+//     username: String
+//     password: String
+// }
 
-type Query{
-    user: [User]
-}
-`;
+// type Query{
+//     user: [User]
+// }
+// `;
 
-const resolvers = {
-  Query: {
-    async user() {
-      return data.users;
-    },
-  },
-};
+// const resolvers = {
+//   Query: {
+//     async user() {
+//       return data.users;
+//     },
+//   },
+// };
 
-async function startApolloServer() {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-  });
-  const { url } = await startStandaloneServer(server, {
-    context: async ({ req, res }) => ({ req, res }),
-    listen: { port: 4000 },
-  });
-  console.log(`
-    🚀  Server is running!
-    📭  Query at ${url}
-    `);
-}
-// startApolloServer();
+// async function startApolloServer() {
+//   const server = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//   });
+//   const { url } = await startStandaloneServer(server, {
+//     context: async ({ req, res }) => ({ req, res }),
+//     listen: { port: 4000 },
+//   });
+//   console.log(`
+//     🚀  Server is running!
+//     📭  Query at ${url}
+//     `);
+// }
+// // startApolloServer();
