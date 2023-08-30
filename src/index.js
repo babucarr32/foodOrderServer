8c75a4e4116948ca2948470815b641f563e0138b
babucarr32 <baboucarry@gmai.com>
2023-08-30 08:44:05 +0000

    send twillio message successful

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import { resolvers } from "../graphql/resolvers.js";
 import { connectDB } from "../utils/connectDB.js";
 import cookieParser from "cookie-parser";
 import express from "express";
+import twilio from "twilio";
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = twilio(accountSid, authToken);
 
 const app = express();
 
@@ -14,6 +19,17 @@ async function connectToDatabase() {
   await connectDB();
 }
 connectToDatabase();
+
+function TwilioMessage() {
+  client.messages
+    .create({
+      body: "This is the ship that made the Kessel Run in fourteen parsecs?",
+      from: "+17076201489",
+      to: "+2203626260",
+    })
+    .then((message) => console.log(message.sid));
+}
+TwilioMessage();
 
 async function startApolloServer() {
   const server = new ApolloServer({
